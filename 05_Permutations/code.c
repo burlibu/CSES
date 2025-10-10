d610fd1b7d51e16a96ec2be7159aec408599157d
burlibu <ale.burlando2005@gmail.com>
2025-10-10 15:11:40 +0200

    Fatto il 5

--- 05_Permutations/code.c
+++ 05_Permutations/code.c
@@ -33,24 +33,65 @@ void db(){
   if (D) printf("Debug\n");
 }
 
+/*
+1. Creare array della permutazione base dato n
+2. Creare due altri array che conteranno i dispari e i pari rispettivamente
+3. Unirli.'
+*/
+
 int main(){
   // permutazione
   long long n;
-  int r = scanf("%lld", &n);
-  if (n == 1) {
-    printf("%lld", n);
-    return 0;
+  int r = scanf("%lld",&n);
+  r = 0;
+  if (n==1){
+    printf("1");
+    return r;
   }
-  char *error = "NO SOLUTION"; // 12 char
-  if (n == 2) {
-    puts(error);
+  if (n<4) {
+    puts("NO SOLUTION");
+    return r;
   }
-  long long resto=0;
-  if (n%2!=0) {
-    n=n-1;
-    resto=n;
-  } // ora so che n è pari
+
+  // Riempimentto array
+  long long *arr = malloc(n*sizeof(long long)); // arr punta all' inizio dell' array
+  long long *start_arr = arr;
   for (long long i = 0; i < n; i++) {
-    printf(s)
+    *(arr+i) = i+1; //=
+    //arr++;
   }
-}
+  long long *arr1; //=
+  long long *arr2; //=
+  long long *start1; //=
+  long long *start2; //=
+  arr1 = malloc(n/2*sizeof(long long));//pari //=
+  if (n%2==0){ //numero di dispari pari
+    arr2 = malloc(n/2*sizeof(long long));//dispari //=
+  }
+  else {// numero di dispari dispari
+    arr2 = malloc(((n/2)+1)*sizeof(long long)); //dispari
+  } //
+  start1 = arr1; //=
+  start2 = arr2; //=
+  for (long long i = 0; i < n;i++) {
+    if (*(arr+i)%2==0) {
+      *arr1 = *(arr+i); // riempio i pari
+      arr1++; // perdo arr1
+    } else {
+      *arr2 = *(arr+i); // riempio i dispari
+      arr2++; //perdo arr2
+    }
+  }
+  //stampa
+  for (long long *p = start1; p < arr1; p++) {
+    printf("%lld ", *p); //p+((n/2)*sizeof(long long))
+  }
+  for (long long *p = start2; p < arr2; p++) {
+    printf("%lld ", *p); //p+(((n/2)-1)*sizeof(long long))
+  }
+  // free(arr1);
+  // free(arr2);
+  // free(start1);
+  // free(start2);
+  return r;
+  };
